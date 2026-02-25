6dae5a0538c807f2c276658db7add02fcc62d039
Janvi Solanki <123942854+janvi-elastic@users.noreply.github.com>
2026-02-25 14:00:35 +0530

    [JupiterOne] - Fix Cannot execute ILM policy delete step (#140049) (#140058)
    This PR focuses on the short term solution which add the `logs-jupiter_one.risks_and_alerts-*` index under the
    kibana_system role with deletion privileges to prevent a failed deletion error when the index enters the
    deletion phase for the ILM lifecycle, in upcoming PR. As it ships transform pipeline too hence read, write permissions
    are also required.
    
    (cherry picked from commit 4646112ae1864630baf9b0537ab6db0b5583a4e3)
    
    # Conflicts:
    #	x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/KibanaOwnedReservedRoleDescriptors.java
    #	x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/security/authz/store/ReservedRolesStoreTests.java

--- x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/KibanaOwnedReservedRoleDescriptors.java
+++ x-pack/plugin/core/src/main/java/org/elasticsearch/xpack/core/security/authz/store/KibanaOwnedReservedRoleDescriptors.java
@@ -563,7 +563,8 @@ class KibanaOwnedReservedRoleDescriptors {
                         TransportDeleteIndexAction.TYPE.name()
                     )
                     .build(),
-                // For ExtraHop, QualysGAV, SentinelOne, Island Browser, Cyera, IRONSCALES and Axonius specific actions.
+                // For ExtraHop, QualysGAV, SentinelOne, Island Browser, Cyera, IRONSCALES, Axonius
+                // and JupiterOne specific actions.
                 // Kibana reads, writes and manages this index
                 // for configured ILM policies.
                 RoleDescriptor.IndicesPrivileges.builder()
@@ -589,7 +590,8 @@ class KibanaOwnedReservedRoleDescriptors {
                         "logs-axonius.network-*",
                         "logs-axonius.storage-*",
                         "logs-axonius.ticket-*",
-                        "logs-axonius.user-*"
+                        "logs-axonius.user-*",
+                        "logs-jupiter_one.risks_and_alerts-*"
                     )
                     .privileges(
                         "manage",
