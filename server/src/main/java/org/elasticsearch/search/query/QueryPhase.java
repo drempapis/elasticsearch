b101b01aaad2c55a6f306e44295e89064174a56b
Dimitris Rempapis <dimitris.rempapis@elastic.co>
2026-07-16 12:50:32 +0300

    update comment

--- server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
+++ server/src/main/java/org/elasticsearch/search/query/QueryPhase.java
@@ -141,9 +141,7 @@ public class QueryPhase {
             return;
         }
 
-        // Pre-process aggregations as late as possible. In the case of a DFS_Q_T_F
-        // request, preProcess is called on the DFS phase, this is why we pre-process them
-        // here to make sure it happens during the QUERY phase
+        // Pre-process aggregations as late as possible, i.e. during the QUERY phase, after the query has been rewritten.
         AggregationPhase.preProcess(searchContext);
 
         addCollectorsAndSearch(searchContext, searchContext.getSearchExecutionContext().getTimeRangeFilterFromMillis(), timeoutRunnable);
